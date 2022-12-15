d72c264e61a5b65b431cdcb047a9d2e53da612a2
Lirosk <kirill_crysis@bk.ru>
2022-12-15 15:52:35 +0300

    Complete kanban endpoints.

--- main/serializers.py
+++ main/serializers.py
@@ -1,7 +1,54 @@
 from rest_framework import serializers
 from . import models
 
+
 class KanbanSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.KanbanEvent
         fields = ["id", "category", "description"]
+
+
+class PatchKanbanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.KanbanEvent
+        fields = ["id", "category", "description", "user"]
+        extra_kwargs = {
+            "id": {
+                "read_only": False
+            }
+        }
+
+    def validate(self, attrs):
+        id = attrs.get("id", 0)
+        existing = models.KanbanEvent.objects.filter(id=id)
+        if not existing.exists():
+            raise serializers.ValidationError("Kanban event with given id doesn't exists.", 400)
+
+        return super().validate(attrs)
+
+
+class PutKanbanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.KanbanEvent
+        fields = ["id", "category", "description", "user"]
+        extra_kwargs = {
+            "user": {
+                "write_only": True
+            }
+        }
+
+    def create(self, validated_data):
+        event = models.KanbanEvent(**validated_data)
+        event.save()
+        return event
+
+
+class DeleteKanbanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.KanbanEvent
+        fields = ["id"]
+        extra_kwargs = {
+            "id": {
+                "read_only": False
+            },
+        }
--- main/views.py
+++ main/views.py
@@ -4,25 +4,94 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from . import serializers
 from . import models
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 # Create your views here.
 
-class KanbanAPIView(GenericAPIView):
-    serializer_class = serializers.KanbanSerializer
 
+class KanbanAPIView(APIView):
+    id = openapi.Parameter("id", in_=openapi.IN_BODY,
+                           type=openapi.TYPE_INTEGER)
+    category = openapi.Parameter(
+        "category", in_=openapi.IN_BODY, type=openapi.TYPE_STRING)
+    description = openapi.Parameter(
+        "description", in_=openapi.IN_BODY, type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(manual_parameters=[])
     def get(self, request):
         events = models.KanbanEvent.objects.filter(user=request.user)
-        serialized = self.serializer_class(events, many=True)
+        serialized = serializers.PatchKanbanSerializer(events, many=True)
+        return Response(serialized.validated_data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Items(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'category': category,
+                    'description': description,
+                }
+            ),
+        ),
+    )
+    def put(self, request):
+        for data in request.data:
+            data["user"] = request.user.email
+        serialized = serializers.PutKanbanSerializer(
+            data=request.data, many=True)
+        serialized.is_valid(raise_exception=True)
+        serialized.save()
         return Response(serialized.data, status=status.HTTP_200_OK)
 
-
-    def put(self, request):
-        ...
-
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Items(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'id': id,
+                    'category': category,
+                    'description': description,
+                }
+            ),
+        ),
+    )
     def patch(self, request):
-        ...
+        for data in request.data:
+            data["user"] = request.user.email
+        serialized = serializers.PatchKanbanSerializer(data=request.data, many=True)
+        serialized.is_valid(raise_exception=True)
+        for obj in serialized.validated_data:
+            event = models.KanbanEvent.objects.get(id=obj["id"])
+            event.category = obj["category"]
+            event.description = obj["description"]
+            event.save()
 
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Items(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'id': id,
+                }
+            ),
+        ),
+    )
     def delete(self, request):
-        ...
+        for data in request.data:
+            data["user"] = request.user.email
+        serialized = serializers.DeleteKanbanSerializer(data=request.data, many=True)
+        serialized.is_valid(raise_exception=True)
+        for obj in serialized.validated_data:
+            existing = models.KanbanEvent.objects.filter(**obj, user=request.user)
+            if not existing.exists():
+                return Response({"message": "Kanban event with given id doesn't exists"}, status=status.HTTP_400_BAD_REQUEST)
+            existing.first().delete()
 
+        return Response(serialized.data, status=status.HTTP_200_OK)
+        # serialized.save()
