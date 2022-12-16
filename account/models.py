03ad55d8884bcae27fa75945dc863588794e1eee
Lirosk <kirill_crysis@bk.ru>
2022-12-16 18:28:45 +0300

    Add table for user redirects.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class Redirect(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    after_password_reset = models.TextField()
